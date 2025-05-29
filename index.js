bf4049b72e8b0178228f24c1a835f0880ac3534a
Maurício Naoki <naokimau@gmail.com>
2025-05-28 23:58:43 -0300

    fix: correção do nome burger

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const burguers = require('./routes/burguers')
-const ingredientes = require("./routes/ingredientes")
-const status = require("./routes/status")
+const burgers = require("./routes/burgers");
+const ingredientes = require("./routes/ingredientes");
+const status = require("./routes/status");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/ingredientes', ingredientes);
-app.use('/api/status', status);
-app.use('/api/burguers', burguers);
+app.use("/api/ingredientes", ingredientes);
+app.use("/api/status", status);
+app.use("/api/burgers", burgers);
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
--- routes/burguers.js
+++ routes/burguers.js
@@ -2,32 +2,32 @@ const express = require("express");
 const { v4: uuid } = require("uuid");
 const router = express.Router();
 
-let burguers = []
+let burgers = [];
 
 router.get("/", (req, res) => {
-    return res.status(200).json(burguers);
+  return res.status(200).json(burgers);
 });
 
 router.post("/", (req, res) => {
-    const novoPedido = {
-        id: uuid(),
-        ...req.body
-    }
+  const novoPedido = {
+    id: uuid(),
+    ...req.body,
+  };
 
-    burguers.push(novoPedido)
-    return res.status(201).json(novoPedido)
-})
+  burgers.push(novoPedido);
+  return res.status(201).json(novoPedido);
+});
 
 router.patch("/:id", (req, res) => {
-    let index = burguers.findIndex(item => item.id === req.params.id)
-    burguers[index].status = req.body.status
-    return res.send("Status atualizado!")
+  let index = burgers.findIndex((item) => item.id === req.params.id);
+  burgers[index].status = req.body.status;
+  return res.send("Status atualizado!");
 });
 
 router.delete("/:id", (req, res) => {
-    let index = burguers.findIndex(item => item.id === req.params.id)
-    burguers.splice(index, 1)
-    return res.send(`Pedido apagado!`)
+  let index = burgers.findIndex((item) => item.id === req.params.id);
+  burgers.splice(index, 1);
+  return res.send(`Pedido apagado!`);
 });
 
 module.exports = router;
